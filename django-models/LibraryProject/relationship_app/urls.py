7d88ba06ce5254c9888a163951a3035c6c295f14
Dawit Sisay <dawitsisay42@example.com>
2025-02-22 03:04:17 -0800

    Added correct URL paths for add_book and edit_book in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
 # relationship_app/urls.py
 
 from django.urls import path
-from .views import add_book, edit_book, delete_book
+from .views import list_books, add_book, edit_book, delete_book
 
 urlpatterns = [
+    path('books/', list_books, name='list_books'),
     path('books/add/', add_book, name='add_book'),
     path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
